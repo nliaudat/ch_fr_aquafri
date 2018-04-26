f37d0d26eea79b4869c2b90662405fecc430031f
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2018-04-26 10:53:44 +0200

    Create pipe_function.sql

---
+++ values/pipe_function.sql
@@ -0,0 +1,3 @@
+INSERT INTO qwat_vl.pipe_function (id, vl_active, short_fr, value_fr, schema_visible, major) VALUES (10001,true,'SP','conduite sprinkler',true,false);
+--INSERT INTO qwat_vl.pipe_function (id, vl_active, short_fr, value_fr, schema_visible, major) VALUES (10002,true,'CI','conduite interne',true,false);
+--INSERT INTO qwat_vl.pipe_function (id, vl_active, short_fr, value_fr, schema_visible, major) VALUES (10003,true,'D','conduite drainante',true,false);
